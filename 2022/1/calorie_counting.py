d2781ed436cd79e165fbbc8052cf712ee3393a0d
flameshikari <mail@hexed.pw>
2022-12-01 21:48:38 +0500

    add day 1 of 2022

---
+++ 2022/1/calorie_counting.py
@@ -0,0 +1,18 @@
+with open('input') as file:
+  inventories = []
+  summary = 0
+  for row in file:
+    if row == '\n':
+      inventories.append(summary)
+      summary = 0
+    else:
+      summary += int(row)
+  inventories.sort()
+
+
+# part 1
+print(f'part 1: {inventories[-1:][0]} calories')
+
+
+# part 2
+print(f'part 2: {sum(inventories[-3:])} calories')
